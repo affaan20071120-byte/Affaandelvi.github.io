c1df01e7ea3fa8da7141065da0c64e217c44aea7
Affaan <affaan20071120@gmail.com>
2025-08-22 15:31:10 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-const io = new IntersectionObserver(entries=>{
-  entries.forEach(e=>{ if(e.isIntersecting){ e.target.classList.add('visible'); } });
-},{ threshold: 0.12 });
-document.querySelectorAll('.card').forEach(el=>io.observe(el));
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('Particles loaded.');
+});
